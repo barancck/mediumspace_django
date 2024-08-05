cf89fbd2ca8f9fd45a418144f7bf0b70d52661fd
baran <barancicek0658@gmail.com>
2024-08-05 17:43:04 +0300

    New Commit

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='Home MediumAI'),
+    path("", views.index, name='index'),
     path("pitchdeck/", views.pitchdeck, name="pitchdeck")
 ]
