ad143b73133d9bea8395d27939fe7fd6d83b57a6
Serhat Keskin <serhatkeskin710@gmail.com>
2024-09-14 12:14:55 +0300

    update

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", views.IndexvView.as_view(), name='index'),
+    # path("", views.index, name='index'),
     path("pitchdeck/", views.pitchdeck, name="pitchdeck")
 ]
--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 from django.contrib import messages
 from django.shortcuts import redirect
-
+from django.views.generic.base import View, TemplateView
 
 
 def index(request): 
@@ -25,6 +25,29 @@ def index(request):
         # return HttpResponse("<h1>Successful</h1>")
     # messages.info(request, 'Welcome to our website!')
     return render(request, "index.html")
+
+class IndexvView(View): 
+    template_name = 'index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        message=request.POST.get('message')
+        contact=Contact()
+        contact.name=name
+        contact.email=email
+        contact.phone=phone
+        contact.message=message
+        contact.save()
+        print("## Data has been written to the database")
+        messages.success(request, 'Your message has been sent successfully!')
+        # return redirect('index')
+        return render(request, "index.html")
+        # return HttpResponse("<h1>Successful</h1>")
 
 def pitchdeck(request):
     return render(request, "pitchdeck.html")
